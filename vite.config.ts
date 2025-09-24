88c006628bfb488fcf95f3fa1c961501b6513a53
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-09-24 15:40:59 +0900

    https으로 연결가능하도록 플러그인 수정 - 수정사항.md 확인

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//   ],
+//   server: {
+//     https: true,
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl' // 플러그인 임포트
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -9,5 +27,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    basicSsl() // 플러그인 추가
   ],
 })
