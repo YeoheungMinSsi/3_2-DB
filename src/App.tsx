d12b7630d5def195bd60c30f8eceafa52f79fd2d
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-11-06 00:54:38 +0900

    Content의 배경을 좌우여백없이 사용하기 위해서 Header부분과 container 부분을 분할

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,22 @@ function App() {
   return (
     <>
       <div id='main'>
-        <div id='container'>
-          <div id="header">
-            <div id="page-Name">홈페이지명</div>
-            <nav className='header-nav-container'>
-              <ul className='header-nav-list'>
-                <li className='header-nav' ><a href="">술 정보</a></li>
-                <li className='header-nav'><a href="">조주정보</a></li>
-              </ul>
-            </nav>
-            <div className='right_header'>
-                {/* <input type="text" /> */}
-              <a href="">로그인</a>
-            </div>
+        <div id="header">
+          <div id="page-Name">홈페이지명</div>
+          <nav className='header-nav-container'>
+            <ul className='header-nav-list'>
+              <li className='header-nav' ><a href="">술 정보</a></li>
+              <li className='header-nav'><a href="">조주정보</a></li>
+            </ul>
+          </nav>
+          <div className='right_header'>
+            {/* <input type="text" /> */}
+            <a href="">로그인</a>
           </div>
-
+        </div>
+        <div id='container'>
+          <div className='content'>
+          </div>
 
         </div>
       </div>
