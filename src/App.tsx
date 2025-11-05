b1e8e4ca385f40d906591789368dfb6f680e30de
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-11-06 00:25:36 +0900

    재시작함 1. 이전에 쓰던 Nav는 임시 휴지통 폴더에 저장 2. App 위에 파일 생성 중 3. UI를 위해서 CSS상에서 width는 100%, max-width는 1440px를 설정함

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 // import { BrowserRouter } from 'react-router-dom';
 import './App.css'
-import Home from './Components/Jsx/Home.tsx'
 
 function App() {
 
   return (
     <>
-      {/* <BrowserRouter>
-        <Home />
-      </BrowserRouter> */}
-      <Router>
-        <Home />
-      </Router>
+      <div id='main'>
+        <div id='container'>
+          <div id="header">
+            <div id="page-Name">홈페이지명</div>
+            <nav className='header-nav-container'>
+              <ul className='header-nav-list'>
+                <li className='header-nav' ><a href="">술 정보</a></li>
+                <li className='header-nav'><a href="">조주정보</a></li>
+              </ul>
+            </nav>
+            <div className='right_header'>
+                {/* <input type="text" /> */}
+              <a href="">로그인</a>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
     </>
   )
 }
@@ -20,6 +32,7 @@ function App() {
 export default App
 
 // import { BrowserRouter as Router } from 'react-router-dom';
+// import Home from './Components/Jsx/Home.tsx'
 
 
 // function App() {
